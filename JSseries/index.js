586f14f0554b7170efa20843d4734972c7d81cc4
Walid <ow_bouhenika@esi.dz>
2025-07-16 14:22:09 +0100

    js

---
+++ JSseries/index.js
@@ -0,0 +1,11 @@
+//console.log("kill yrs");
+
+//window.alert("ye jsut say so  ");
+let x = 125;
+let y = true;
+let z = "hiiii";
+
+document.getElementById("myh").textContent = x;
+document.getElementById("p1").textContent = y;
+
+document.getElementById("p2").textContent = z;
