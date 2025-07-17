62d46e3512cac9c504ea561e710fa49fed2ed23a
Walid <ow_bouhenika@esi.dz>
2025-07-17 14:36:51 +0100

    capital problems

--- JSseries/index.js
+++ JSseries/index.js
@@ -2,10 +2,11 @@
 let username;
 console.log(username);
 let howmanytimes = 0;
+const x = Math.PI;
 
 document.getElementById("submitform").onclick = function () {
 	username = document.getElementById("userform").value;
-	document.getElementById("user").textContent = ` UPDATED ${username}`;
+	document.getElementById("user").textContent = ` UPDATED ${username} ${x}`;
 	howmanytimes++;
 	document.getElementById("times").textContent = `${howmanytimes}`;
 };
