205f08e95fedd8da745ed5347b4fdd1f7ef3bea3
Walid <ow_bouhenika@esi.dz>
2025-07-18 14:52:33 +0100

    go do something then comeback

--- JSseries/index.js
+++ JSseries/index.js
@@ -1,2 +1,13 @@
-let username = "had";
-console.log("HAD".length);
+let Name = "Walid Bouhenika";
+let firstname = Name.slice(0, Name.indexOf(" "));
+console.log(firstname);
+let lastname = Name.slice(Name.indexOf(" ") + 1, Name.length);
+console.log(lastname);
+//now standarlizing
+let username = window.prompt("please type a username");
+username =
+	username.trim().charAt(0).toUpperCase() +
+	username.slice(1, username.length).toLowerCase().replaceAll(" ", "_") +
+	"_" +
+	username.length;
+console.log(username);
