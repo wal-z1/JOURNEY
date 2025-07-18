407207aaefc7176552d15e7c4dc2c33b6507fd50
Walid <ow_bouhenika@esi.dz>
2025-07-18 13:39:49 +0100

    change to the if statment

--- JSseries/check ifs/index.js
+++ JSseries/check ifs/index.js
@@ -7,11 +7,10 @@ const submit = document.getElementById("submiter");
 const msg1 = document.getElementById("MSG1");
 const msg2 = document.getElementById("MSG2");
 submit.onclick = function () {
-	if (check.checked) {
-		msg1.textContent = "checked";
-	} else {
-		msg1.textContent = "not checked";
-	}
+	check.checked
+		? (msg1.textContent = "checked")
+		: (msg1.textContent = "not checked");
+
 	if (like.checked) {
 		msg2.textContent = "like";
 	} else if (love.checked) {
