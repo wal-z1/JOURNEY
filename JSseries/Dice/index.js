08fb5962e81b80cc1697a038ddcd43ab59e091fd
Walid <ow_bouhenika@esi.dz>
2025-07-22 13:05:17 +0100

    it works idc im not doing the css

--- JSseries/Dice/index.js
+++ JSseries/Dice/index.js
@@ -4,11 +4,20 @@ const DiceIT = [];
 const DiceNT = [];
 
 function DiceFunc() {
+	DiceNT.length = 0; // Clears the arrays each time
+	DiceIT.length = 0;
 	const NUMofDice = document.getElementById("num").value;
 	/*works here better until click then we read*/
-	for (i = 1; i < NUMofDice; i++) {
+	for (i = 1; i <= NUMofDice; i++) {
+		/*filled an array of randomly genrate numbers*/
 		let x = Math.floor(Math.random() * 6 + 1);
 		DiceNT.unshift(x);
-		console(DiceNT);
+		/*link the path to the element*/
+		DiceIT.unshift(`<img src="dice-${x}.png" alt="Dice Images" >`);
 	}
+	/*debuggin*/
+	console.log(...DiceNT);
+	console.log(...DiceIT);
+	Diceres.textContent = DiceNT.join(" ,  ");
+	Diceimg.innerHTML = DiceIT.join("  ");
 }
