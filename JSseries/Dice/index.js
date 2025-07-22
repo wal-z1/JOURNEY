580e1ec3d1b3c2d2325e6ea4d9888d2829662e9b
Walid <ow_bouhenika@esi.dz>
2025-07-22 12:39:14 +0100

    Js

--- JSseries/Dice/index.js
+++ JSseries/Dice/index.js
@@ -1,3 +1,14 @@
-function DiceFunc(){
-  
+const Diceres = document.getElementById("Diceres");
+const Diceimg = document.getElementById("Diceimg");
+const DiceIT = [];
+const DiceNT = [];
+
+function DiceFunc() {
+	const NUMofDice = document.getElementById("num").value;
+	/*works here better until click then we read*/
+	for (i = 1; i < NUMofDice; i++) {
+		let x = Math.floor(Math.random() * 6 + 1);
+		DiceNT.unshift(x);
+		console(DiceNT);
+	}
 }
