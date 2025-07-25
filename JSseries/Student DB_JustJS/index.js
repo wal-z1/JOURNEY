99855154ad7810d9ca26e2047e374fdf4294171f
Walid <ow_bouhenika@esi.dz>
2025-07-25 16:48:58 +0100

    finish mini prj later

--- JSseries/Student DB_JustJS/index.js
+++ JSseries/Student DB_JustJS/index.js
@@ -1,3 +1,4 @@
+//defining classes
 class Students {
 	static sscount = 0;
 
@@ -6,6 +7,14 @@ class Students {
 		this.year = year;
 		this.courses = courses;
 		Students.sscount++; // use class name to access static
+	}
+}
+class Course {
+	constructor(name, level, grade, coef = 1) {
+		this.name = name;
+		this.level = level;
+		this.grade = grade;
+		this.coef = coef;
 	}
 }
 
@@ -38,7 +47,17 @@ const student2 = new Students(
 		grade: 13,
 	}
 );
+// a fucntion that calculates the gpa (no coef for now)
+function gpac(student) {
+	//calculates a total using reduce
+	let total = student.courses.reduce((acc, subject) => acc + subject.grade, 0);
+	let NfCourses = student.courses.length;
+	return total / NfCourses;
+}
+//with coef gpa
+function gpa(student) {}
 
 console.log(student1);
 console.log(student2);
-console.log(Students.sscount);
+console.log(Students.sscount, "Number Of Students for now");
+console.log(gpac(student1));
